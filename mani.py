08aaefcdc80c7c01ed2689f41f5e8b9b70b58b07
mariopad <159138817+mariopad@users.noreply.github.com>
2024-02-15 19:36:04 +0100

    Rotaciones
    He terminado de añadir las rotaciones. Es operativa la función de ejecución de los movimientos en términos del array que escupe kociemba.
    He corregido algunos bugs y he mejorado la función print para que sean más claras las modificaciones introducidas por las rotaciones.

--- mani.py
+++ mani.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#He definido esta func para estudiar un poco las simetrías en las rotaciones, pero numpy tiene la suya: np.rot90()
 def rot_90(A, times):
     rotated_A = np.zeros((A.shape[0], A.shape[0]), dtype=object)
     transposed_A = np.transpose(A)  # np.flipud(np.fliplr(A))
@@ -35,8 +36,29 @@ class CuboRubik:
     #    self.down = np.array([[27, 28, 29], [30, 31, 32], [33, 34, 35]])
     #    self.right = np.array([[36, 37, 38], [39, 40, 41], [42, 43, 44]])
     #    self.left = np.array([[45, 46, 47], [48, 49, 50], [51, 52, 53]])
-
+    #def __init__(self):
+    #    self.left = np.array([[_ for _ in range(3)] for _ in range(3)])
+    #    self.front = np.array([[_ for _ in range(3)] for _ in range(3)])
+    #    self.right = np.array([[_ for _ in range(3)] for _ in range(3)])
+    #    self.back = np.array([[_ for _ in range(3)] for _ in range(3)])
+    #    self.up = np.array([[_ for _ in range(3)] for _ in range(3)])
+    #    self.down = np.array([[_ for _ in range(3)] for _ in range(3)])
     def F(self, times=-1):
+        #Rotación cara ppal
+        self.front = rot_90(self.front, times)
+
+        #Definición elementos adyacentes
+        left_column = self.left[:, 2].copy()
+        right_column = self.right[:, 0].copy()
+        up_row = self.up[2, :].copy()
+        down_row = self.down[0, :].copy()
+
+        #Modificación elementos adyacentes
+        self.left[:, 2] = down_row
+        self.right[:, 0] = up_row
+        self.up[2, :] = np.flipud(left_column)
+        self.down[0, :]  = np.flipud(right_column)
+    def Fp(self, times=1):
         self.front = rot_90(self.front, times)
 
         left_column = self.left[:, 2].copy()
@@ -44,51 +66,226 @@ class CuboRubik:
         up_row = self.up[2, :].copy()
         down_row = self.down[0, :].copy()
 
-        self.left[:, 2] = down_row
-        self.right[:, 0] = up_row
-        self.up[2, :] = np.flipud(left_column)
-        self.down[0, :] = np.flipud(right_column)
+        self.left[:, 2]  = np.flipud(up_row)
+        self.right[:, 0] = np.flipud(down_row)
+        self.up[2, :]    = right_column
+        self.down[0, :]  = left_column
 
+    def B(self, times=-1):
+        self.back = rot_90(self.back, times)
+
+        left_column = self.right[:, 2].copy()
+        right_column = self.left[:, 2].copy()
+        up_row = np.flipud(self.up[0, :].copy())
+        down_row = np.flipud(self.down[2, :].copy())
+
+        self.right[:, 2] = np.flipud(down_row)
+        self.left[:, 2] = np.flipud(up_row)
+        self.up[0, :] = left_column
+        self.down[2, :] = right_column
+
+    def Bp(self, times=1):
+        self.back = rot_90(self.back, times)
+
+        left_column = self.right[:, 2].copy()
+        right_column = self.left[:, 2].copy()
+        up_row = np.flipud(self.up[0, :].copy())
+        down_row = np.flipud(self.down[2, :].copy())
+
+        self.left[:, 2] = up_row
+        self.right[:, 2] = down_row
+        self.up[0, :] = np.flipud(right_column)
+        self.down[2, :] = np.flipud(left_column)
+
+    def L(self, times=-1):
+        self.left = rot_90(self.left, times)
+
+        left_column = self.back[:, 2].copy()
+        right_column = self.front[:, 0].copy()
+        up_row = self.up[:, 0].copy()
+        down_row = np.flipud(self.down[:, 0].copy())
+
+        self.back[:, 2] = np.flipud(down_row)
+        self.front[:, 0] = up_row
+        self.up[:, 0] = np.flipud(left_column)
+        self.down[:, 0] = right_column
+
+    def Lp(self, times=1):
+        self.left = rot_90(self.left, times)
+
+        left_column = self.back[:, 2].copy()
+        right_column = self.front[:, 0].copy()
+        up_row = self.up[:, 0].copy()
+        down_row = np.flipud(self.down[:, 0].copy())
+
+        self.back[:, 2] = np.flipud(up_row)
+        self.front[:, 0] = down_row
+        self.up[:, 0] = right_column
+        self.down[:, 0] = np.flipud(left_column)
+
+    def R(self, times=-1):
+        self.right = rot_90(self.right, times)
+
+        left_column = self.front[:, 2].copy()
+        right_column = self.back[:, 0].copy()
+        up_row = np.flipud(self.up[:, 2].copy())
+        down_row = self.down[:, 2].copy()
+
+        self.front[:, 2] = down_row
+        self.back[:, 0] = np.flipud(up_row)
+        self.up[:, 2] = left_column
+        self.down[:, 2] = np.flipud(right_column)
+
+    def Rp(self, times=1):
+        self.right = rot_90(self.right, times)
+
+        left_column = self.front[:, 2].copy()
+        right_column = self.back[:, 0].copy()
+        up_row = np.flipud(self.up[:, 2].copy())
+        down_row = self.down[:, 2].copy()
+
+        self.front[:, 2] = up_row
+        self.back[:, 0] = np.flipud(down_row)
+        self.up[:, 2] = np.flipud(right_column)
+        self.down[:, 2] = left_column
+
+    def U(self, times=-1):
+        self.up = rot_90(self.up, times)
+
+        left_column = self.left[0, :].copy()
+        right_column = np.flipud(self.right[0, :].copy())
+        up_row = np.flipud(self.back[0, :].copy())
+        down_row = self.front[0, :].copy()
+
+        self.left[0, :] = down_row
+        self.right[:, 0] = up_row
+        self.back[0, :] = left_column
+        self.front[0, :] = right_column
+
+    def Up(self, times=1):
+        self.front = rot_90(self.front, times)
+
+        left_column = self.left[0, :].copy()
+        right_column = np.flipud(self.right[0, :].copy())
+        up_row = np.flipud(self.back[0, :].copy())
+        down_row = self.front[0, :].copy()
+
+        self.left[0, :] = up_row
+        self.right[:, 0] = down_row
+        self.back[0, :] = right_column
+        self.front[0, :] = left_column
+
+    def D(self, times=-1):
+        self.down = rot_90(self.down, times)
+
+        left_column = np.flipud(self.left[2, :].copy())
+        right_column = self.right[2, :].copy()
+        up_row = self.front[2, :].copy()
+        down_row = np.flipud(self.down[2, :].copy())
+
+        self.left[2, :] = down_row
+        self.right[2, :] = up_row
+        self.front[2, :] = left_column
+        self.down[2, :] = right_column
+
+    def Dp(self, times=1):
+        self.down = rot_90(self.down, times)
+
+        left_column = np.flipud(self.left[2, :].copy())
+        right_column = self.right[2, :].copy()
+        up_row = self.front[2, :].copy()
+        down_row = np.flipud(self.down[2, :].copy())
+
+        self.left[:, 2] = up_row
+        self.right[:, 0] = down_row
+        self.up[2, :] = right_column
+        self.down[0, :] = left_column
+
+    """
+    Importante: Kociemba dará una lista de la forma
+    kociemba_str = "R' D2 R' U2 R F2 D B2 U' R F' U R2 D L2 D' B2 R2 B2 U' B2"
+    
+    Para que podamos trabajar con ella:
+    kociemba_lst = kociemba_str.split()
+    """
     def execute_moves(self, moves):
+        #Diccionario para identificar la salida de Kociemba con las rotaciones
         move_mapping = {
             'F': self.F,
             "F'": self.Fp,
-            'vacavoladora': self.vacavoladora
-            # poner las demás rotaciones
+            "F2": lambda: [self.F() for _ in range(2)],
+
+            'B': self.B,
+            "B'": self.Bp,
+            "B2": lambda: [self.B() for _ in range(2)],
+
+            'L': self.L,
+            "L'": self.Lp,
+            "L2": lambda: [self.L() for _ in range(2)],
+
+            'R': self.R,
+            "R'": self.Rp,
+            "R2": lambda: [self.R() for _ in range(2)],
+
+            'U': self.U,
+            "U'": self.Up,
+            "U2": lambda: [self.U() for _ in range(2)],
+
+            'D': self.D,
+            "D'": self.Dp,
+            "D2": lambda: [self.D() for _ in range(2)]
         }
 
+        #Ejecución de rotaciones en orden de array
         for move in moves:
             move_function = move_mapping.get(move)
             if move_function:
                 move_function()
             else:
-                print(f"Error: No se encontró la función asociada a la cadena '{move}'")
+                print(f"Error: No se encontró '{move}'")
 
-    def print_cube(self):
-            # Imprimir el diseño del cubo
-            #print('   U')
-            #print('L  F  R  B')
-            #print('   D')
+    """
+    Esta función guarra pinta el cubo como viene siendo habitual:
+       U
+    L  F  R  B
+       D
+    """
+    def print_state(self):
+        for i in range(3):
+            print("\t\t\t\t\t\t|", end=" ")
+            for j in range(3):
+                print(f"{self.up[i][j]}", end=" ")
+            print("|")
+        print("-------------------------------------------------------------------------------------------------------------")
+        for i in range(3):
+            print("|", end=" ")
+            for j in range(3):
+                print(f"{self.left[i][j]}", end=" ")
+            print("|", end=" ")
+            for j in range(3):
+                print(f"{self.front[i][j]}", end=" ")
+            print("|", end=" ")
+            for j in range(3):
+                print(f"{self.right[i][j]}", end=" ")
+            print("|", end=" ")
+            for j in range(3):
+                print(f"{self.back[i][j]}", end=" ")
+            print("|")
+        print("-------------------------------------------------------------------------------------------------------------")
+        for i in range(3):
+            print("\t\t\t\t\t\t|", end=" ")
+            for j in range(3):
+                print(f"{self.down[i][j]}", end=" ")
+            print("|")
 
-            # Imprimir las caras del cubo
-            print('\nCara U:')
-            print(self.up)
-            print('\nCara L:')
-            print(self.left)
-            #print('\nCara F:')
-            #print(self.front)
-            print('\nCara R:')
-            print(self.right)
-            #print('\nCara B:')
-            #print(self.back)
-            print('\nCara D:')
-            print(self.down)
-
-
-# Crear un cubo de Rubik
+# Testeo
 cubo = CuboRubik()
+#Prueba de exmoves
+#kociemba_array = ["F", "F'", "F2"]
+#cubo.execute_moves(kociemba_array)
+
 # Imprimir el diseño del cubo
-cubo.print_cube()
-cubo.F()
+cubo.print_state()
+cubo.Fp()
 print('\n')
-cubo.print_cube()
+cubo.print_state()
